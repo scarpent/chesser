2643ab9e1dc397df47a2a76bd8c887a1cd28c405
Scott Carpenter <scottc9000@gmail.com>
2025-01-25 11:08:41 -0600

    Configure DB and format

--- chesser/tests/test_home.py
+++ chesser/tests/test_home.py
@@ -1,7 +1,8 @@
-from django.urls import reverse
 from django.test import Client
+from django.urls import reverse
+
 
 def test_home_page():
     client = Client()
-    response = client.get(reverse('home'))
+    response = client.get(reverse("home"))
     assert response.status_code == 200
--- chesser/urls.py
+++ chesser/urls.py
@@ -22,5 +22,5 @@ from chesser.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', home, name='home'),
+    path("", home, name="home"),
 ]
