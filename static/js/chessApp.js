8bfd9bd7559d408cde0fbaecf507ec931f938cca
Scott Carpenter <scottc9000@gmail.com>
2025-02-07 14:06:32 -0600

    Move stuff into chessApp.js

---
+++ static/js/chessApp.js
@@ -0,0 +1,32 @@
+export function chessApp() {
+  return {
+    board: null,
+    chess: null,
+    status: "Ready",
+    initChessground() {
+      const boardElement = document.getElementById("board");
+      if (boardElement && window.Chessground && window.Chess) {
+        this.chess = new window.Chess();
+
+        this.board = window.Chessground(boardElement, {
+          viewOnly: false,
+          draggable: true,
+          highlight: {
+            lastMove: true,
+            check: true,
+          },
+          orientation: "black",
+          movable: {
+            color: "both", // Allow both white and black to move
+            free: true, // Any moves
+            showDests: false,
+          },
+        });
+
+        this.status = "Chessboard Loaded!";
+      } else {
+        console.error("Chessground or Chess.js failed to load");
+      }
+    },
+  };
+}
