1554376d53d43babba3d1fad26bc16aede18f3b5
Scott Carpenter <scottc9000@gmail.com>
2025-02-07 18:39:25 -0600

    Adding stuff back: toDests, handleMove

--- chesser/views.py
+++ chesser/views.py
@@ -14,10 +14,14 @@ def nav(request):
 def practice(request):
     # Example JSON data structure
     quiz_data = {
+        "color": "white",
+        "start": 0,  # opening state before opposing move
+        "end": 4,  # or 99?
         "moves": [
             {
                 "fen": "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
                 "move": "e4",
+                "alt": ["d4", "Nf3", "c4"],
             },
             {
                 "fen": "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",
@@ -26,8 +30,20 @@ def practice(request):
             {
                 "fen": "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
                 "move": "Nf3",
+                "alt": ["d5", "Nc3"],  # accepted good moves that don't fail the quiz
+                "alt_fail": ["d5"],  # accepted good moves that fail the quiz
             },
-        ]
+            {
+                "fen": "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3",  # noqa: E501
+                "move": "Nc6",
+            },
+            {
+                "fen": "r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3",  # noqa: E501
+                "move": "d4",
+                "alt": ["Nc3", "Bc4", "Bb5"],
+                "alt_fail": [],
+            },
+        ],
     }
 
     context = {"quiz_data": json.dumps(quiz_data)}
--- static/js/chessApp.js
+++ static/js/chessApp.js
@@ -9,11 +9,17 @@ export function chessApp() {
     board: null,
     chess: null,
     status: "Ready",
+    quizData: quizData,
+
     initChessground() {
       const boardElement = document.getElementById("board");
       if (boardElement && window.Chessground && window.Chess) {
         this.chess = new window.Chess();
 
+        const first = this.quizData.moves[this.quizData.start]
+        console.log(first.fen, first.move)
+
+        this.chess.load(first.fen);
         this.board = window.Chessground(boardElement, {
           viewOnly: false,
           draggable: true,
@@ -21,17 +27,58 @@ export function chessApp() {
             lastMove: true,
             check: true,
           },
-          orientation: "black",
+          orientation: this.quizData.color,
+          fen: first.fen,
           movable: {
             color: "both", // Allow both white and black to move
-            free: true, // Any moves
+            free: false, // Only legal moves
+            dests: this.toDests(),
             showDests: false,
+            events: {
+              after: this.handleMove.bind(this),
+            },
           },
         });
 
-        this.status = "Chessboard Loaded!";
+        this.status = "Chessboard loaded!";
       } else {
-        console.error("Chessground or Chess.js failed to load");
+        console.error("Chessground or chess.js failed to load");
+      }
+    },
+    toDests() {
+      const dests = new Map();
+      const squares =
+        this.chess.SQUARES ||
+        this.chess
+          .board()
+          .flatMap((row) =>
+            row.map((square) => (square ? square.square : null))
+          )
+          .filter(Boolean);
+      squares.forEach((s) => {
+        const ms = this.chess.moves({ square: s, verbose: true });
+        if (ms.length)
+          dests.set(
+            s,
+            ms.map((m) => m.to)
+          );
+      });
+      return dests;
+    },
+    handleMove(orig, dest) {
+      const move = this.chess.move({ from: orig, to: dest });
+      if (move) {
+        this.status = `Moved from ${orig} to ${dest}`;
+        this.board.set({
+          fen: this.chess.fen(),
+          movable: {
+            dests: this.toDests(),
+          },
+        });
+
+        // this.checkQuizMove(this.chess.fen());
+      } else {  // this won't happen because of "free: false"
+        this.status = `Illegal move from ${orig} to ${dest}`;
       }
     },
   };
