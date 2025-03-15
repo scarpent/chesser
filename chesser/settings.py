1da330cbe289606b100960ca5d7ac38765450d3b
Scott Carpenter <scottc9000@gmail.com>
2025-03-15 12:33:24 -0500

    Always run whitenoise for static

--- chesser/settings.py
+++ chesser/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",  # always run whitenoise (dev/prod)
     "django_extensions",
     "chesser",
 ]
