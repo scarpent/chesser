139128fc2f7151983a585220460905a64225bb5a
Scott Carpenter <scottc9000@gmail.com>
2025-03-18 17:29:57 -0500

    30 minute schedule, larger "next" quiz button

--- chesser/tasks.py
+++ chesser/tasks.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 def start_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(backup_and_upload, "interval", minutes=1)
+    scheduler.add_job(backup_and_upload, "interval", minutes=30)
     scheduler.start()
 
 
