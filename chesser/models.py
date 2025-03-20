777f66e862d446af89d314c0750cc18b251972f8
Scott Carpenter <scottc9000@gmail.com>
2025-03-20 10:37:13 -0500

    Update model __str__ methods

--- chesser/models.py
+++ chesser/models.py
@@ -20,22 +20,17 @@ class Course(models.Model):
     title = models.CharField(max_length=100)
     color = models.CharField(max_length=5)
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
 
 
 class Chapter(models.Model):
     title = models.CharField(max_length=100)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     course_title = "(no course?!)"
-    #     try:
-    #         course_title = self.course.title
-    #     except Exception as e:
-    #         print(f"chapter id {self.id} error accessing course.title", e)
-
-    #     return f"{course_title}: {self.title}"
+    def __str__(self):
+        course_title = self.course.title if self.course_id else "(no course)"
+        return f"{course_title}: {self.title}"
 
 
 class Variation(models.Model):
@@ -74,19 +69,10 @@ class Variation(models.Model):
             ),
         ]
 
-    # def __str__(self):
-    #     course_title = "(no course?!)"
-    #     try:
-    #         course_title = self.chapter.course.title
-    #     except Exception as e:
-    #         print(f"variation id {self.id} error accessing chapter.course.title", e)
-
-    #     try:
-    #         course_title = self.course.title
-    #     except Exception as e:
-    #         print(f"variation id {self.id} error accessing course.title", e)
-
-    #     return f"{course_title}: {self.chapter.title}: {self.title} ({self.id})"
+    def __str__(self):
+        course_title = self.course.title if self.course_id else "(no course)"
+        chapter_title = self.chapter.title if self.chapter_id else "(no chapter)"
+        return f"{course_title}: {chapter_title}: {self.title} ({self.id})"
 
     @property
     def mainline_moves(self):  # TODO: maybe don't need this anymore...
@@ -197,11 +183,11 @@ class Move(models.Model):
         unique_together = ("variation", "sequence")
         ordering = ["sequence"]
 
-    # def __str__(self):
-    #     return (
-    #         f"{self.variation.id}: {self.variation.title}: "
-    #         f"{self.move_num} {self.san}"
-    #     )
+    def __str__(self):
+        return (
+            f"{self.variation.title} ({self.variation_id}): "
+            f"{self.move_num} {self.san}"
+        )
 
     @property
     def move_verbose(self):
