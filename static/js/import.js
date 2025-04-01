801129d1956d470e637f8920415ff56390b77a44
Scott Carpenter <scottc9000@gmail.com>
2025-03-31 22:51:29 -0500

    Populate import form with local date

--- static/js/import.js
+++ static/js/import.js
@@ -1,12 +1,15 @@
 export function importApp() {
   return {
     chapters: window.importData?.chapters || [],
+    // en-CA is English (Canada) official format (YYYY-MM-DD)
+    // this is the required html format and is widely supported
+    // but not universally by all browsers
     formDefaults: {
       ...window.importData?.form_defaults,
       start_move: 2,
       next_review_date:
         window.importData?.form_defaults?.next_review_date ||
-        new Date().toISOString().split("T")[0],
+        new Date().toLocaleDateString("en-CA"),
     },
 
     // Alpine's x-model struggles to restore select values
