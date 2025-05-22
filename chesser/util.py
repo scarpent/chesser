f4a8844d4d29ccd2787f40230ed337e9cf126233
Scott Carpenter <scottc9000@gmail.com>
2025-05-21 22:25:54 -0500

    The mariner's tale goes on and on and on

--- chesser/util.py
+++ chesser/util.py
@@ -54,8 +54,10 @@ def get_time_ago(now, result_datetime):
 
     if seconds < 15 * 60:
         return "just now"
-    if seconds < 52 * 60:
+    if seconds < 50 * 60:
         return plural("minute", int(seconds // 60))
+    if seconds < 70 * 60:
+        return "about an hour ago"
     if seconds < 24 * 60 * 60:
         return plural("hour", int(seconds // 3600))
     if days < 14:
